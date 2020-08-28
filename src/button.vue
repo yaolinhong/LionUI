c7ac29106fd47d0e202130154685df4d79f97363
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2020-08-28 23:08:13 +0800

    g-button添加icon、icon-position属性

--- src/button.vue
+++ src/button.vue
@@ -1,33 +1,48 @@
 <template>
-<!--  单文件组件-->
-  <button class="g-button">按钮</button>
+  <!--  单文件组件-->
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <div class="content"><slot></slot></div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  props: ["icon","iconPosition"]
+
+}
 </script>
 <style lang="scss">
+
+
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
-  /*不要写width*/
+  vertical-align: middle;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;justify-content: center;align-items: center;
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: var(--bo rder-color-hover);
   }
-
   &:active {
     background-color: var(--button-active-bg);
-
   }
-
   &:focus {
     outline: none;
   }
+  > .content{order: 2;}
+  > .icon {order:1;margin-right: .4em}
 
-}
+  &.icon-right{
+    > .content {order:1;}
+    > .icon{order:2;margin-right:0;margin-left: .4em;}
+    }
+  }
+
 
 
 
