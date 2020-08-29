09ed91061e3ff8fac76941bf1efdc3a22e8a81f6
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2020-08-29 13:36:25 +0800

    完成button-group

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 import Vue from 'vue/dist/vue.js';
 import Button from './button.vue';
 import Icon from './icon'
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
+import ButtonGroup from "./button-group.vue"
 
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component("g-button-group",ButtonGroup)
 
 new Vue({
-    el:"#app",
-    data:{
-        loading1:false,
-        loading2:false,
-        loading3:false,
+    el: "#app",
+    data: {
+        loading1: false,
+        loading2: false,
+        loading3: false,
     }
 })
+//单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+{
+    //console.log(Button) Button是一个对象
+    const Constructor = Vue.extend(Button)
+    //Constructor是一个函数，通过这个函数构造一个button
+    const button = new Constructor({
+        propsData: {
+            icon: "settings"
+        }
+    })
+    //通过js把按钮挂载到页面
+    button.$mount()
+    //button里面的元素找use元素
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement)
+    let href = useElement.getAttribute("xlink:href")
+    expect(href).to.eq("#icon-settings")
+}
---
+++ src/button-group.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="g-button-group">
+  <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  mounted() {
+    for (let node of this.$el.children){
+      console.log(node.nodeName)//BUTTON
+      let name=node.nodeName.toLowerCase()
+      if (name !=='button'){
+        console.warn(`g-button-group的子元素应该全是button，但是你写了${{name}}`)}
+    }
+  }
+}
+</script>
+<style lang="scss" >
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  border:0;
+  > .g-button{
+    border-radius:0;
+    &:not(:first-child){margin-left: -1px;}
+    &:first-child {
+    border-top-left-radius: var(--border-radius)  ;
+    border-bottom-left-radius:var(--border-radius) ;}
+    &:last-child {
+      border-top-right-radius: var(--border-radius)  ;
+      border-bottom-right-radius:var(--border-radius) ;
+    }
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+  }
+
+   }
+</style>
