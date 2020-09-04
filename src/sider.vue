84e4ade4cbc42d455d7ae13040419ce28542abb3
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2020-09-04 20:09:21 +0800

    完成布局组件

--- src/sider.vue
+++ src/sider.vue
@@ -1,25 +1,43 @@
 <template>
+  <transition name="slide-fade">
   <div class="sider" v-if="visible">
-    <button class="close" @click="visible=false" >x</button>
+    <button class="close" @click="toggle">关闭侧边框</button>
     <slot/>
   </div>
-</template>
-<script >
-export default {
-  data(){
-    return {visible:true}
-  },
-  methods(){
+    </transition>
 
+</template>
+<script>
+export default {
+  data() {
+    return {visible: true}
+  },
+  methods: {
+    toggle() {
+      this.visible = !this.visible;
+    }
   }
 }
 </script>
 <style lang="scss">
-.sider{
-  position:relative;
+.sider {
+  position: relative;
 }
-.close{
-  position:absolute;
+
+.close {
+  position: absolute;
   right: 0;
 }
+
+.slide-fade-enter-active {
+  transition: all 0.8s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to{
+  transform: translateX(-10px);
+  opacity: 0;
+}
+
 </style>
